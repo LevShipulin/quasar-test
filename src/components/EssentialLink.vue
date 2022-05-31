90354d5c21b49248799f2d43e705e0a8d628eceb
Lev Shipulin <site@shipulin.info>
2022-05-31 08:01:53 +0200

    init

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-item clickable tag="a" target="_blank" v-ripple>
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="props.icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  caption: String,
+  link: String,
+  icon: String,
+});
+</script>
